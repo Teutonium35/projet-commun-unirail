460385787b3505c12089b4bec6492b2a114bdf46
Mathieu HERESBACH <teutonium35@gmail.com>
2025-03-07 13:42:22 +0100

    fix: comment on can_infra.c

--- app/utility/src/can_infra.c
+++ app/utility/src/can_infra.c
@@ -3,7 +3,7 @@
 int set_switch_straight(int aig_name){
     int can_dlc = 3;
     int command_mode = 'W'; // For positioning, L to lock, U to unlock, I to reset
-    int turn_mode = 0x01; // For turn, 0x01 for straight
+    int turn_mode = 0x01; // For straight, 0x00 for straight
     int data[8] = {command_mode,turn_mode,0,0,0,0,0,0};
     return send_can_data(aig_name, can_dlc, data);
 }
