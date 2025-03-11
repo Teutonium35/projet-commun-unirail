4d108efe09b373747fadae75200cd9f79d2bbfe5
azremos <antoine.daguerre2@gmail.com>
2025-03-11 11:14:25 +0100

    Ajout constantes de chemins/ressources

---
+++ app/utility/src/const_chemins.c
@@ -0,0 +1,17 @@
+#include "../../EVC/include/automatique.h"
+#include "../include/const_chemin.h"
+
+const int L_res_req1[] = [9,3]
+const int L_res_lib1[] = [1,12]
+const unsigned char L_mask_req1[] = [0b00001,0b01000]
+const unsigned char L_mask_lib1[] = [0b00001,0b01000]
+
+const int L_res_req2[] = [21,17,11]
+const int L_res_lib2[] = [17,11,251,20]
+const unsigned char L_mask_req2[] = [0b00010,0b00100,0b11000]
+const unsigned char L_mask_lib2[] = [0b00001,0b01000,0b01000,0b10000]
+
+const int L_res_req3[] = [24,29,11,9]
+const int L_res_lib3[] = [27,11,12,22]
+const unsigned char L_mask_req3[] = [0b10000,0b00100,0b01000,0b00011]
+const unsigned char L_mask_lib3[] = [0b10000,0b00100,0b01000,0b00011]
