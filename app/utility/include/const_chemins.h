516f1fc7d06935783945e9fbf2fa0725d36d37c6
MathieuH35 <teutonium35@gmail.com>
2025-03-11 11:37:37 +0100

    fix: const chemins declarations

--- app/utility/include/const_chemins.h
+++ app/utility/include/const_chemins.h
@@ -1,17 +1,24 @@
-#include "../../EVC/include/automatique.h"
-#include "../include/const_chemin.h"
+#ifndef CHEMINS
+    #define CHEMINS
+    #include "../../EVC/include/automatique.h"
 
-extern const int L_res_req1[] = [9,3]
-extern const int L_res_lib1[] = [1,12]
-extern const unsigned char L_mask_req1[] = [0b00001,0b01000]
-extern const unsigned char L_mask_lib1[] = [0b00001,0b01000]
+    extern const int L_res_req1[];
+    extern const int L_res_lib1[];
+    extern const unsigned char L_mask_req1[];
+    extern const unsigned char L_mask_lib1[];
 
-extern const int L_res_req2[] = [21,17,11]
-extern const int L_res_lib2[] = [17,11,251,20]
-extern const unsigned char L_mask_req2[] = [0b00010,0b00100,0b11000]
-extern const unsigned char L_mask_lib2[] = [0b00001,0b01000,0b01000,0b10000]
+    extern const int L_res_req2[];
+    extern const int L_res_lib2[];
+    extern const unsigned char L_mask_req2[];
+    extern const unsigned char L_mask_lib2[];
 
-extern const int L_res_req3[] = [24,29,11,9]
-extern const int L_res_lib3[] = [27,11,12,22]
-extern const unsigned char L_mask_req3[] = [0b10000,0b00100,0b01000,0b00011]
-extern const unsigned char L_mask_lib3[] = [0b10000,0b00100,0b01000,0b00011]
+    extern const int L_res_req3[];
+    extern const int L_res_lib3[];
+    extern const unsigned char L_mask_req3[];
+    extern const unsigned char L_mask_lib3[];
+
+    extern const int * L_res_req[3];
+    extern const int * L_res_lib[3];
+    extern const int * L_mask_req[3];
+    extern const int * L_mask_lib[3];
+#endif
--- app/utility/src/const_chemins.c
+++ app/utility/src/const_chemins.c
@@ -1,17 +1,22 @@
 #include "../../EVC/include/automatique.h"
-#include "../include/const_chemin.h"
+#include "../include/const_chemins.h"
 
-const int L_res_req1[] = [9,3]
-const int L_res_lib1[] = [1,12]
-const unsigned char L_mask_req1[] = [0b00001,0b01000]
-const unsigned char L_mask_lib1[] = [0b00001,0b01000]
+const int L_res_req1[2] = {9,3};
+const int L_res_lib1[2] = {1,12};
+const unsigned char L_mask_req1[2] = {0b00001,0b01000};
+const unsigned char L_mask_lib1[2] = {0b00001,0b01000};
 
-const int L_res_req2[] = [21,17,11]
-const int L_res_lib2[] = [17,11,251,20]
-const unsigned char L_mask_req2[] = [0b00010,0b00100,0b11000]
-const unsigned char L_mask_lib2[] = [0b00001,0b01000,0b01000,0b10000]
+const int L_res_req2[3] = {21,17,11};
+const int L_res_lib2[4] = {17,11,251,20};
+const unsigned char L_mask_req2[3] = {0b00010,0b00100,0b11000};
+const unsigned char L_mask_lib2[4] = {0b00001,0b01000,0b01000,0b10000};
 
-const int L_res_req3[] = [24,29,11,9]
-const int L_res_lib3[] = [27,11,12,22]
-const unsigned char L_mask_req3[] = [0b10000,0b00100,0b01000,0b00011]
-const unsigned char L_mask_lib3[] = [0b10000,0b00100,0b01000,0b00011]
+const int L_res_req3[4] = {24,29,11,9};
+const int L_res_lib3[4] = {27,11,12,22};
+const unsigned char L_mask_req3[4] = {0b10000,0b00100,0b01000,0b00011};
+const unsigned char L_mask_lib3[4] = {0b10000,0b00100,0b01000,0b00011};
+
+const int * L_res_req[3] = {L_res_req1, L_res_req2, L_res_req2};
+const int * L_res_lib[3] = {L_res_lib1, L_res_lib2, L_res_lib2};
+const int * L_mask_req[3] = {L_mask_req1, L_mask_req2, L_mask_req2};
+const int * L_mask_lib[3] = {L_mask_lib1, L_mask_lib2, L_mask_lib2};
