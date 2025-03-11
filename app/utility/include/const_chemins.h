6761cc585633196e654c69665c2c841de5a2f139
MathieuH35 <teutonium35@gmail.com>
2025-03-11 14:09:07 +0100

    fix: var types for compiler

--- app/utility/include/const_chemins.h
+++ app/utility/include/const_chemins.h
@@ -4,18 +4,18 @@
 
     extern const int L_res_req1[];
     extern const int L_res_lib1[];
-    extern const unsigned char L_mask_req1[];
-    extern const unsigned char L_mask_lib1[];
+    extern const int L_mask_req1[];
+    extern const int L_mask_lib1[];
 
     extern const int L_res_req2[];
     extern const int L_res_lib2[];
-    extern const unsigned char L_mask_req2[];
-    extern const unsigned char L_mask_lib2[];
+    extern const int L_mask_req2[];
+    extern const int L_mask_lib2[];
 
     extern const int L_res_req3[];
     extern const int L_res_lib3[];
-    extern const unsigned char L_mask_req3[];
-    extern const unsigned char L_mask_lib3[];
+    extern const int L_mask_req3[];
+    extern const int L_mask_lib3[];
 
     extern const int * L_res_req[3];
     extern const int * L_res_lib[3];
