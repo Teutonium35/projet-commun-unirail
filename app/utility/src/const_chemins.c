d2653c6f96af14d5b6d73b3d65983a5b286a1376
MathieuH35 <teutonium35@gmail.com>
2025-03-13 12:30:25 +0100

    fix: res mask index from 122 to 123

--- app/utility/src/const_chemins.c
+++ app/utility/src/const_chemins.c
@@ -16,8 +16,8 @@ const int L_res_lib3[4] = {27,11,12,22};
 const int L_mask_req3[4] = {0b10000,0b00100,0b01000,0b00011};
 const int L_mask_lib3[4] = {0b10000,0b00100,0b01000,0b00011};
 
-const int * L_res_req[3] = {L_res_req1, L_res_req2, L_res_req2};
-const int * L_res_lib[3] = {L_res_lib1, L_res_lib2, L_res_lib2};
-const int * L_mask_req[3] = {L_mask_req1, L_mask_req2, L_mask_req2};
-const int * L_mask_lib[3] = {L_mask_lib1, L_mask_lib2, L_mask_lib2};
+const int * L_res_req[3] = {L_res_req1, L_res_req2, L_res_req3};
+const int * L_res_lib[3] = {L_res_lib1, L_res_lib2, L_res_lib3};
+const int * L_mask_req[3] = {L_mask_req1, L_mask_req2, L_mask_req3};
+const int * L_mask_lib[3] = {L_mask_lib1, L_mask_lib2, L_mask_lib3};
 const int L_res_size[3] = {2, 3, 4};
